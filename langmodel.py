a645c506c6dbbcbc51bc976ae9a831c19d056025
Sagar B Hathwar <sagarbhathwar@gmail.com>
2017-03-19 14:42:15 +0000

    Cross entropy

---
+++ langmodel.py
@@ -0,0 +1,75 @@
+from collections import Counter
+from functools import reduce
+from copy import deepcopy
+
+import numpy as np
+import nltk
+
+
+class LangaugeModel:
+
+    def __init__(self, bookname):
+        self.bookname = bookname
+        self.author = bookname.split('-')[0]
+        
+        self.unigrams = list(nltk.corpus.gutenberg.words(bookname))
+        self.bigrams = list(nltk.bigrams(self.unigrams))
+        self.trigrams = list(nltk.trigrams(self.unigrams))
+
+        self.unigrams_c = Counter(self.unigrams)
+        self.bigrams_c = Counter(self.bigrams)
+        self.trigrams_c = Counter(self.trigrams)
+
+        self.unigrams_p = {
+            w: self.unigrams_c[w] / len(self.unigrams) for w in self.unigrams}
+        self.bigrams_p = {
+            w: self.bigrams_c[w] / self.unigrams_c[w[0]] for w in self.bigrams}
+        self.trigrams_p = {
+            w: self.trigrams_c[w] / self.bigrams_c[w[0:2]] for w in self.trigrams}
+
+        self.smoothed_unigrams = deepcopy(self.unigrams)
+        self.smoothed_bigrams = deepcopy(self.bigrams)
+        self.smoothed_trigrams = deepcopy(self.trigrams)
+
+        self.smoothed_unigrams_c = {}
+        self.smoothed_bigrams_c = {}
+        self.smoothed_trigrams_c = {}
+
+        self.smoothed_unigrams_p = {}
+        self.smoothed_bigrams_p = {}
+        self.smoothed_trigrams_p = {}
+
+    def smooth(self, unigrams, bigrams, trigrams):
+        """
+        Add one smoothing
+        Each unigram, bigram and trigram contain all those ever possible in the 
+        three books. So, updating each of them with 'everything' makes sure that ones which
+        are already present is incremented by one while the rest are initialized to one
+        Also the smoothed probabilities is calculated in a way similar to unsmoothed models 
+        """
+        self.smoothed_unigrams += unigrams
+        self.smoothed_bigrams += bigrams
+        self.smoothed_trigrams += trigrams
+        
+        self.smoothed_unigrams_c = Counter(self.smoothed_unigrams)
+        self.smoothed_bigrams_c = Counter(self.smoothed_bigrams)
+        self.smoothed_trigrams_c = Counter(self.smoothed_trigrams)
+
+        
+        n = len(self.smoothed_unigrams)
+        self.smoothed_unigrams_p = {
+            w: self.smoothed_unigrams_c[w] / n for w in self.smoothed_unigrams}
+        self.smoothed_bigrams_p = {
+            w: self.smoothed_bigrams_c[w] / self.smoothed_unigrams_c[w[0]] for w in self.smoothed_bigrams}
+        self.smoothed_trigrams_p = {
+            w: self.smoothed_trigrams_c[w] / self.smoothed_bigrams_c[w[0:2]] for w in self.smoothed_trigrams}
+
+    def calculate_cross_entropy_values(self):
+        """
+        returns the cross entorpy computer for unigram, bigram
+        and trigram models w.r.t each other for this language model
+        """
+        u_ce = (-1 / len(self.unigrams)) * np.sum(np.log([self.unigrams_p[w] for w in self.unigrams]))
+        b_ce = (-1 / len(self.bigrams)) * np.sum(np.log([self.bigrams_p[w] for w in self.bigrams]))
+        t_ce = (-1 / len(self.trigrams)) * np.sum(np.log([self.trigrams_p[w] for w in self.trigrams]))
+        print(u_ce, b_ce, t_ce)
---
+++ main.py
@@ -0,0 +1,17 @@
+from langmodel import LangaugeModel
+
+C1 = LangaugeModel('austen-emma.txt')
+C2 = LangaugeModel('edgeworth-parents.txt')
+C3 = LangaugeModel('whitman-leaves.txt')
+
+ug_set = set(C1.unigrams) | set(C2.unigrams) | set(C3.unigrams)
+bg_set = set(C1.bigrams) | set(C2.bigrams) | set(C3.bigrams)
+tg_set = set(C1.trigrams) | set(C2.trigrams) | set(C3.trigrams)
+
+C1.smooth(ug_set, bg_set, tg_set)
+C2.smooth(ug_set, bg_set, tg_set)
+C3.smooth(ug_set, bg_set, tg_set)
+
+C1.calculate_cross_entropy_values()
+C2.calculate_cross_entropy_values()
+C3.calculate_cross_entropy_values()
